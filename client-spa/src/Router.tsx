df6239caf5e42a90e58dd4712be2f089dc47ae9c
Tej Birring <tejbirring@gmail.com>
2024-12-23 13:52:07 +0000

    Update router (paths) and the main menu component

--- client-spa/src/Router.tsx
+++ client-spa/src/Router.tsx
@@ -3,47 +3,96 @@ import {
     RouterProvider,
   } from "react-router-dom";
 import Root from "./routes/Root";
-import Home from "./routes/feature:family-tree-entry-and-visualisation/Home";
-import Error from "./routes/Error";
-import MarkdownContent from "./routes/feature:markdown-content/MarkdownContent";
+import HomeSermo from "./routes/feature:family-tree-entry-and-visualisation/HomeSermo";
+import HomeCodex from "./routes/feature:family-tree-entry-and-visualisation/HomeCodex";
+import ErrorElement from "./routes/Error";
+import MarkdownContent from "./components/feature:markdown-content/MarkdownContent";
+import Home from "./routes/Home";
+import HomeAtrium from "./routes/feature:family-tree-directory/HomeAtrium";
+import Novitas from "./routes/Novitas";
+import store from "./store";
+import { fetchContentNewsItems } from "./content/slice";
+import { Box } from "@mantine/core";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <Error />,
+    errorElement: <ErrorElement />,
     children: [
       {
-          path: "",
-          element: <Home />
+        path: "",
+        element: <Home />
+      },
+      // Novitates -> Novitas
+      {
+        path: "novitas/:recordId",
+        element: <Novitas />,
+        async loader({params}) {
+          await store.dispatch(fetchContentNewsItems());
+          const state = store.getState();
+          const recordId = params.recordId;
+          const record = state.content.newsItems?.find((r)=>r.id === recordId);    
+          if (record) {
+            return {
+              record
+            }
+          } else {
+            throw new Error(`Novitas not found: ${recordId}`);
+          }
+        }
+      },
+      // Sermo
+      {
+        path: "sermo",
+        element: <HomeSermo />
+      },
+      // Codex
+      {
+          path: "codex",
+          element: <HomeCodex />
       },
       {
-          path: "users-guide",
-          element: <MarkdownContent content={import("./content/markdown/users-guide.md?raw")} />
+        path: "codex/notation",
+        element: (
+          <Box px={{base: 0, sm:"1rem"}}>
+            <MarkdownContent content={import("./content/markdown/codex-notation.md?raw")} />
+          </Box>
+        )
+      },
+      // Atrium Familiarum
+      {
+        path: "atrium",
+        element: <HomeAtrium />
+      },
+      // About Us
+      {
+        path: "about/principia",
+        element: (
+            <Box px={{base: 0, sm:"1rem"}}>
+              <MarkdownContent content={import("./content/markdown/about/principia.md?raw")} />
+            </Box>
+          )
       },
       {
-        path: "about/what-is-tapestry•family",
-        element: <MarkdownContent content={import("./content/markdown/about/what-is-tapestry•family.md?raw")} />
+        path: "about/auguria",
+        element: (
+            <Box px={{base: 0, sm:"1rem"}}>
+              <MarkdownContent content={import("./content/markdown/about/auguria.md?raw")} />
+            </Box>
+          )
       },
+      // Terms and Conditions
       {
-        path: "about/our-mission",
-        element: <MarkdownContent content={import("./content/markdown/about/our-mission.md?raw")} />
-      },
-      {
-        path: "about/our-story",
-        element: <MarkdownContent content={import("./content/markdown/about/our-story.md?raw")} />
-      },
-      {
-        path: "about/product-roadmap",
-        element: <MarkdownContent content={import("./content/markdown/about/product-roadmap.md?raw")} />
-      },
-      {
-        
+        path: "terms/advertisement",
+        element: (
+            <Box px={{base: 0, sm:"1rem"}}>
+              <MarkdownContent content={import("./content/markdown/terms-and-conditions/advertisement.md?raw")} />
+            </Box>
+          )
       }
-      // {
-      //   path: "not-users-guide",
-      //   element: <MarkdownContent content={import("./content/markdown/not-users-guide.md?raw")} />
-      // }
+      // Policies
+      // ...
     ]
   },
 ]);
--- client-spa/src/components/TheMainMenu.tsx
+++ client-spa/src/components/TheMainMenu.tsx
@@ -5,19 +5,25 @@ export default function TheMenuMain() {
     return (
             <Flex direction="column" justify="space-between" h="100%" p="sm" styles={{root: {overflowY: "auto"}}}>
                 <div>
-                    <NavLink label="The Atelier (Home)" href="/" />
-                    <NavLink label="User's Guide" href="/users-guide" />
-
+                    <NavLink label="Scribe ut..." description="Record a lineage here">
+                        <NavLink label="Codex" description="Enter a lineage using precise notation" href="/codex" />
+                        <NavLink label="Sermo" description="Enter a lineage using everyday words (EXPERIMENTAL)" href="/sermo" />
+                    </NavLink>
+                    <NavLink label="Atrium Familiarum" description="Discover ancestral lineages" href="/atrium" />
                     <Divider my="sm" />
 
-                    <NavLink label="About">
-                        <NavLink label="What is Tapestry•Family?" href="/about/what-is-tapestry•family" />
-                        <NavLink label="Our Mission" href="/about/our-mission" />
-                        <NavLink label="Our Story" href="/about/our-story" />
-                        <NavLink label="Product Roadmap" href="/about/product-roadmap" />
+                    <NavLink label="About Us">
+                        <NavLink label="Principia" 
+                        description="Our foundations and purpose"
+                        href="/about/principia" 
+                    />
+                        <NavLink label="Auguria" 
+                        description="Our vision for the future, including product roadmap, planned features, and more"
+                        href="/about/auguria" 
+                    />
                     </NavLink>
 
-                    <Button w="100%" mt="sm" color='navy.6'>Become a Patron</Button>
+                    <Button w="100%" mt="sm" color='navy.6'>Support our Work</Button>
 
                     <Divider my="sm" />
 
@@ -29,7 +35,7 @@ export default function TheMenuMain() {
 
                 <div>
                     <Divider mb="sm" />
-                    <NavLink label="Contact Us" />
+                    <NavLink label="Contact Us" href="https://airtable.com/apph4b8oGTIzV49FI/pag2MWYvUqluRA7fD/form" target='_blank' />
                 </div>
             </Flex>
     )

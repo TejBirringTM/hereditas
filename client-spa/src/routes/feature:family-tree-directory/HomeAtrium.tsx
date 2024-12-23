494abb9f435258c80b2731dbc6ff53282a0f213e
Tej Birring <tejbirring@gmail.com>
2024-12-23 12:33:19 +0000

    Add Atrium Familiarum page

---
+++ client-spa/src/routes/feature:family-tree-directory/HomeAtrium.tsx
@@ -0,0 +1,128 @@
+import { AspectRatio, Box, Button, Card, Divider, Flex, Grid, Image, Pill, Text, Title } from "@mantine/core";
+import { useEffect } from "react";
+import { fetchContentCodexExamples } from "../../content/slice";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store";
+import CalendarIcon from "../../assets/icons/uicons-solid-straight/fi-ss-calendar-day.svg?react";
+import UserIcon from "../../assets/icons/uicons-solid-straight/fi-ss-user.svg?react";
+import PageTitle from "../../components/PageTitle";
+import { Carousel } from "@mantine/carousel";
+import { useNavigate } from "react-router-dom";
+import { codexShareLink } from "../feature:family-tree-entry-and-visualisation/libs/codex-share-link";
+
+export default function HomeAtrium() {
+    const dispatch = useDispatch<AppDispatch>();
+    const navigate = useNavigate();
+
+    const records = useSelector((state: RootState)=>{
+        return state.content.codexExamples;
+     });
+
+     useEffect(()=>{
+        void dispatch(fetchContentCodexExamples());
+     });
+
+     function navigateToCodex(token: string) {
+        const link = codexShareLink(token, true);
+        navigate(link);
+     }
+
+    return (
+        <Box px={{base: 0, sm:"1rem"}}>
+            <PageTitle 
+                title="Attrium Familiarum"
+                titleColour="softer-warm.9"
+                description="Explore ancestries compiled and curated by distinguished Hereditas scholars and visitors." 
+                descriptionColour="navy.7"
+            />
+            
+            <Divider my="lg" />
+
+            <Grid gutter={{base: "lg", md: "lg"}} mb="lg">
+            {
+                records?.map((r)=>(
+                    <Grid.Col span={{base: 12, lg: 6, xl: 4}}>
+
+                        <Card shadow="md" bg="softer-warm.9" c="white">
+                            <Card.Section py="sm" px="md">
+                                {
+                                    r.Images && r.Images.length > 0 &&
+                                    <Card.Section mb="md" bg="neutral.1">
+                                            <Carousel withIndicators style={{cursor: "ew-resize"}}>
+                                            { 
+                                                r.Images.map((image, idx)=>(
+                                                    <Carousel.Slide key={`${r.id}-img#${idx+1}`}>
+                                                        <AspectRatio ratio={1080 / 720} mx="auto">
+                                                            <Image
+                                                                src={image.url}
+                                                                alt={`${r.Title} Image ${idx+1}`}
+                                                                fit="cover"
+                                                            />
+                                                        </AspectRatio>
+                                                    </Carousel.Slide>
+                                                ))
+                                            }
+                                            </Carousel>
+                                    </Card.Section>
+                                }
+                                <Title order={5} fw="bold" lh={0.89} style={{textAlign: "center"}}>{r.Title}</Title>
+                                {   r["Alternative Titles"] &&
+                                    <Text size="xs" style={{textAlign: "center"}}>
+                                        <em>Alt. </em>
+                                        {r['Alternative Titles']}
+                                    </Text>
+                                }
+                            </Card.Section>
+
+
+                            <Card.Section bg="gray.1" p="lg" withBorder>
+                                <Text lh={1.25} size="sm" c="navy.9" pb="xs">
+                                    {r.Description}
+                                </Text>                                
+                                
+                                    {r.Tags && r.Tags.length > 0 &&
+                                        <Flex gap="xs" mt="sm">
+                                            {r.Tags.map((tag)=>(
+                                                <Pill size="xs" bg="navy.5" c="white">{tag}</Pill>
+                                            ))}
+                                        </Flex>
+                                    }
+                                
+                            </Card.Section>
+
+
+                            <Card.Section bg="gray.1">
+                            <Flex direction="row" justify="space-between" align="center" py="xs" pl="lg" pr="md">     
+                                    <Flex direction="row" gap="sm">    
+                                        <Flex direction="row" align="center" gap="0.3rem" c="neutral.9" opacity={0.9}>
+                                            <CalendarIcon fill="currentColor" height={20} width={20} opacity={0.76} />
+                                            <Flex direction="column">
+                                                <Text size="xs" lh={1} style={{fontVariant: "small-caps"}} fw="bold">Published</Text>
+                                                <Text size="xs">{new Date(r.Created).toLocaleDateString()}</Text>
+                                            </Flex>
+                                        </Flex>
+                                        {
+                                            r["Presenter's Name"] &&
+                                            <Flex direction="row" align="center" gap="0.3rem" c="neutral.9" opacity={0.9}>
+                                                <UserIcon fill="currentColor" height={20} width={20} opacity={0.76} />
+                                                <Flex direction="column">
+                                                    <Text size="xs" lh={1} style={{fontVariant: "small-caps"}} fw="bold">Author</Text>
+                                                    <Text size="xs">{r["Presenter's Name"]}</Text>
+                                                </Flex>
+                                            </Flex>
+                                        }
+                                    </Flex>
+
+                                    <Button size="xs" variant="filled" color="softer-warm.9" onClick={()=>navigateToCodex(r.token)}>View</Button>
+                                </Flex>
+                            </Card.Section>
+                            
+                        </Card>
+
+                    </Grid.Col>
+                ))
+            }
+            </Grid>
+        </Box>
+    )
+}

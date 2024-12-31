d75d86bfb15184a6bd7744ba6acd4ed958b5a96a
Tej Birring <tejbirring@gmail.com>
2024-12-31 09:43:38 +0000

    Update app content models

--- server/services/app-content/model/table-codex-submissions.ts
+++ server/services/app-content/model/table-codex-submissions.ts
@@ -7,11 +7,11 @@ export const recordSchema_codexSubmission = v.object({
     Description: v.optional(v.string()),
     Codex: v.optional(v.string()),
     "Presenter Name": v.optional(v.string()),
-    "Presenter Email": v.optional(v.string()),
-    "Hide Presenter Email": v.optional(v.boolean()),
+    "Presenter's Email": v.optional(v.string()),
+    "Hide Presenter's Email": v.optional(v.boolean()),
     Created: v.string(),
     "Last Modified": v.optional(v.string()),
     "Approved": v.optional(v.boolean())
 });
 
-export const tableCodexSubmissions = new AirtableSync("apph4b8oGTIzV49FI", "tblz7CyEockMSBhlw", {ttlInMilliseconds: config.cache.ttlInMilliseconds, selectOptions: { filterByFormula: "{Approved} = TRUE()" }}, recordSchema_codexSubmission);
+export const tableCodexSubmissions = new AirtableSync("apph4b8oGTIzV49FI", "tblz7CyEockMSBhlw", {ttlInMilliseconds: config.cache.ttlInMilliseconds, selectOptions: { filterByFormula: "{Approved} = TRUE()" }}, recordSchema_codexSubmission, (input)=>input);

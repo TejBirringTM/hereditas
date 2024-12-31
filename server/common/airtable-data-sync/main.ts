32833eea5f40f26e4f0c31e6d6c4bc6d9e928b74
Tej Birring <tejbirring@gmail.com>
2024-12-31 10:11:19 +0000

    Fix typing

--- server/common/airtable-data-sync/main.ts
+++ server/common/airtable-data-sync/main.ts
@@ -12,29 +12,38 @@ interface TableSyncOptions {
 export class AirtableSync<
   TEntries extends v.ObjectEntries,
   TMessage extends v.ErrorMessage<v.ObjectIssue> | undefined,
+  TOut
 > {
     private readonly baseId: string;
     private readonly tableId: string;
     private readonly selectOptions: SelectOptions<FieldSet>;
     private readonly schema: v.ObjectSchema<TEntries, TMessage>;
     private readonly ttl: number;
-    private _records : v.InferObjectOutput<TEntries>[] = [];
+    private readonly transform : (input: v.InferOutput<typeof this.schema>) => TOut;
+    private _records : TOut[];
     private lastFetchTimestamp : number;
-
-    constructor(baseId: string, tableId: string, options: TableSyncOptions, schema: v.ObjectSchema<TEntries, TMessage>) {
+    
+    constructor(baseId: string, tableId: string, options: TableSyncOptions, schema: v.ObjectSchema<TEntries, TMessage>, transform?: (input: v.InferOutput<v.ObjectSchema<TEntries, TMessage>>) => TOut) {
         this.baseId = baseId;
         this.tableId = tableId;
         this.selectOptions = options.selectOptions ?? {};
+        this.transform = transform ?? ((input)=>input as TOut);
         this.ttl = options.ttlInMilliseconds;
         this.schema = schema;
         this.lastFetchTimestamp = 0;
+        this._records = [];
     }
 
     private async fetch() {
-        debugOnly(()=>{
+        debugOnly(()=>{ 
             console.debug(`Syncing 'airtable:${this.baseId}:${this.tableId}'`);
         })
-        this._records = await airtableRecords(this.baseId, this.tableId, this.selectOptions, this.schema);
+        const records = await airtableRecords(this.baseId, this.tableId, this.selectOptions, this.schema);
+        this._records = [];
+        for await (const r of records) {
+            const transformedRecord = await this.transform(r);
+            this._records.push(transformedRecord);
+        }
         this.lastFetchTimestamp = Date.now();
         debugOnly(()=>{
             console.debug(`Done syncing 'airtable:${this.baseId}:${this.tableId}' @ ${this.lastFetchTimestamp}`);

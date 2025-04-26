895a24c1f5e9cbfbf5b3967fe2f54264c9ae3314
Tej Birring <tejbirring@gmail.com>
2025-04-26 18:28:12 +0100

    Add example env file.

--- client-spa/src/main.tsx
+++ client-spa/src/main.tsx
@@ -12,20 +12,27 @@ import { MantineProvider } from '@mantine/core'
 import defaultTheme from './assets/themes/default-theme'
 const posthogApiKey = import.meta.env.VITE_POSTHOG_PUBLIC_API_KEY as string;
 const posthogHostUrl = import.meta.env.VITE_POSTHOG_HOST_URL as string;
-posthog.init(
-  posthogApiKey,
-  {
-    api_host: posthogHostUrl,
-    capture_heatmaps: true,
-    debug: isDebugMode(),
-    person_profiles:  "always",
-    autocapture: false,
-    disable_session_recording: false,
-    capture_performance: true,
-    ip: true,
-    enable_recording_console_log: true
-  }
-);
+
+if (posthogApiKey && posthogHostUrl) {
+  console.debug("Initialising analytics...");
+  posthog.init(
+    posthogApiKey,
+    {
+      api_host: posthogHostUrl,
+      capture_heatmaps: true,
+      debug: isDebugMode(),
+      person_profiles:  "always",
+      autocapture: false,
+      disable_session_recording: false,
+      capture_performance: true,
+      ip: true,
+      enable_recording_console_log: true
+    }
+  );
+} else {
+  console.debug("Skip initialising analytics.");
+}
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
